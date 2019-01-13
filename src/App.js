50cb4e9b55cb4f3fa2e4dfc952823943ce790215
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-01-13 12:21:24 +0600

    Rewrited App.jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "antd/dist/antd.css";
 import "./App.css";
-import { Layout } from "./components";
+import { Layout, SearchInput } from "./components";
+import axios from "axios";
+
+const API_KEY = "7009910c";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.getSearch = this.getSearch.bind(this);
+  }
+
+  state = {
+    results: []
+  };
+
+  async getSearch(s) {
+    const result = await axios.get(
+      `http://www.omdbapi.com?apikey=${API_KEY}&s=${s}`
+    );
+    this.setState({
+      results: result.data.Search
+    });
+  }
+
   render() {
-    return <Layout>h</Layout>;
+    return (
+      <Layout>
+        <SearchInput onSearch={this.getSearch} />
+        {this.state.results.map(m => (
+          <div>
+            <img src={m.Poster} alt="" />
+          </div>
+        ))}
+      </Layout>
+    );
   }
 }
 
