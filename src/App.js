a011c5733743e6b97c04fa085baf84c93452fffb
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-01-13 10:52:47 +0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "antd/dist/antd.css";
+import "./App.css";
+import { Layout } from "./components";
+
+class App extends Component {
+  render() {
+    return <Layout>h</Layout>;
+  }
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Layout from "./Layout";
+import ListItem from "./ListItem";
+import SearchInput from "./SearchInput";
+
+export { Layout, ListItem, SearchInput };
